97b3cb4ccd60f88e9f1d6a937160cbe6b2788dee
Keshav Sundararaman <ikeshav42@gmail.com>
2024-12-15 13:41:55 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-  test: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
